0848f65f20d63d256759f40f7d09ee950ff9bd9b
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2021-05-10 22:51:14 +0200

    2021

--- index.js
+++ index.js
@@ -6,3 +6,24 @@
         index++;
     }
 })();
+
+var body = document.querySelector('body');
+var _2020 = document.querySelector('._2020');
+var _2021 = document.querySelector('._2021');
+
+if (Date.now() < new Date('2021-05-12T00:00:01.000+02:00')) {
+    _2021.classList.add('hidden');
+} else {
+    _2020.classList.add('hidden');
+}
+
+body.classList.remove('hidden');
+
+body.addEventListener('click', toggleMessage);
+body.addEventListener('touchstart', toggleMessage);
+
+function toggleMessage() {
+    [_2020, _2021].forEach(message => {
+        message.classList.toggle('hidden');
+    });
+}
